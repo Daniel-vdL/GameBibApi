71995b3828b2f65642cd939acaf8a2bcb55c2974
justinvanderhoeven <74967919+justinvanderhoeven@users.noreply.github.com>
2024-03-26 10:07:57 +0100

    Update AppDbContext.cs
    I dont know if this is correct

--- GameBibApi/Models/AppDbContext.cs
+++ GameBibApi/Models/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using GameBibApi.Models;
+﻿using GameBib.Models;
+using GameBibApi.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 using System.Runtime.CompilerServices;
@@ -16,10 +17,26 @@ namespace GameBibApi.Models
             optionsBuilder.UseMySql(
                 System.Configuration.ConfigurationManager.ConnectionStrings["GameBibApi"].ConnectionString,
                 ServerVersion.Parse("5.7.33-winx64"));
+
+            //var steam = System.Configuration.ConfigurationManager.AppSettings["steamKey"];
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            modelBuilder.Entity<WantedGame>()
+           .HasKey(wg => wg.Id);
+
+            modelBuilder.Entity<WantedGame>()
+              .HasOne(wg => wg.app)
+               .WithMany(g => g.WantedByUsers)
+               .HasForeignKey(fg => fg.GameId);
+
+            modelBuilder.Entity<WantedGame>()
+                  .HasOne(wg => wg.userDto)
+                  .WithMany(u => u.WantedGames)
+                  .HasForeignKey(wg => wg.UserId);
+
             modelBuilder.Entity<User>().HasData(
                 new User { Id = 1, Username = "Dani", statusId = 0, Password = SecureHasher.Hash("1234")},
                 new User { Id = 2, Username = "Justin", statusId = 0, Password = SecureHasher.Hash("1234")});

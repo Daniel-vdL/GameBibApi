fa7484625e0f169c537946dc878e03a8fcb4dc11
justinvanderhoeven <74967919+justinvanderhoeven@users.noreply.github.com>
2024-03-26 10:08:05 +0100

    Create AppList.cs

---
+++ GameBibApi/Models/AppList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBib.Models
+{
+    public class App
+    {
+        public int appid { get; set; }
+        public string name { get; set; }
+        public ICollection<WantedGame> WantedByUsers { get; set; }
+    }
+
+    public class AppList
+    {
+        public List<App> apps { get; set; }
+    }
+
+    public class RootObject
+    {
+        public AppList applist { get; set; }
+    }
+}

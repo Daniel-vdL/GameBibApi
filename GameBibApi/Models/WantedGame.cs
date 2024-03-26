2792eaf4cad183695dc3dbfc684f34e813909802
justinvanderhoeven <74967919+justinvanderhoeven@users.noreply.github.com>
2024-03-26 10:05:34 +0100

    app.config + wantedgame class

---
+++ GameBibApi/Models/WantedGame.cs
@@ -0,0 +1,19 @@
+﻿using GameBibApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBib.Models
+{
+    public class WantedGame
+    {
+        public int Id { get; set; }
+        public int AppId { get; set; }
+        public int UserId { get; set; }
+
+        public  App app { get; set; }
+        public UserDto userDto { get; set; }
+    }
+}
